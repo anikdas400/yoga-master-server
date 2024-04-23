97134e0162801bf6d5c1c039d9ea091b56ab122a
anikdas400 <anikchandra400@gmail.com>
2024-04-23 16:55:38 +0600

    complite post and get method

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+// midleware
+app.use(cors());
+app.use(express.json())
+
+// mongodb connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-master.tp08lqq.mongodb.net/?retryWrites=true&w=majority&appName=yoga-master`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create a database and collection
+    const database = client.db("yoga-master");
+    const userCollection = database.collection("users");
+    const classCollection = database.collection("classes");
+    const cartCollection = database.collection("carts");
+    const paymentCollection = database.collection("payments");
+    const enrolledCollection = database.collection("enrolled");
+    const appliedCollection = database.collection("applied");
+
+    // classes route here
+    app.post('/new-class',async(req,res)=>{
+      const newClass = req.body;
+      // newClass.availableSeats = parseInt(newClass.availableSeats);
+      const result = await classCollection.insertOne(newClass)
+      res.send(result)
+
+    })
+
+    app.get('/classes', async(req,res)=>{
+      const query = { status:"approved" }
+      const result = await classCollection.find().toArray();
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Developers 2025!')
+})
+
+app.listen(port, () => {
+  console.log(`Yoga-Master listening on port ${port}`)
+})
