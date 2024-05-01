a23254417f44eab4f76158c585de73930edf747c
anikdas400 <anikchandra400@gmail.com>
2024-05-02 04:45:02 +0600

    classes route complite with classCollection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 // mongodb connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-master.tp08lqq.mongodb.net/?retryWrites=true&w=majority&appName=yoga-master`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,18 +37,94 @@ async function run() {
     const enrolledCollection = database.collection("enrolled");
     const appliedCollection = database.collection("applied");
 
-    // classes route here
+    // CLASSES ROUTE HERE
+    
     app.post('/new-class',async(req,res)=>{
       const newClass = req.body;
       // newClass.availableSeats = parseInt(newClass.availableSeats);
       const result = await classCollection.insertOne(newClass)
       res.send(result)
 
-    })
+    });
+
+    // get all classes
 
     app.get('/classes', async(req,res)=>{
       const query = { status:"approved" }
       const result = await classCollection.find().toArray();
+      res.send(result)
+    });
+
+    // get classes by instructor email address
+
+    app.get('/classes/:email', async(req,res)=>{
+      const email = req.params.email ;
+      const query = {instructorEmail: email};
+      const result = await classCollection.find(query).toArray();
+      res.send(result)
+    });
+
+    // manage classes
+
+    app.get('/manage-classes', async(req,res)=>{
+      const result = await classCollection.find().toArray();
+      res.send(result)
+    });
+
+    // update classes status and reason
+
+    app.patch('/change-status/:id',async(req,res)=>{
+      const id = req.params.id;
+      const status = req.body.status;
+      const reason = req.body.reason;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status,
+          reason: reason,
+        },
+      };;
+      const result = await classCollection.updateOne(filter,updateDoc,options);
+      res.send(result);
+    });
+
+    // get approved classes
+
+    app.get('/approved-classes', async(req,res)=>{
+      const query = { status:"approved" };
+      const result = await classCollection.find(query).toArray();
+      res.send(result)
+    });
+
+    // get signle class details
+
+    app.get('/class/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await classCollection.findOne(query);
+      res.send(result)
+    })
+
+    // update class details (all data)
+
+    app.put('/update-class/:id', async(req,res)=>{
+      const id = req.params.id;
+      const updateClass = req.body;
+      const filter ={_id: new ObjectId(id)};
+      const options = { upsert : true};
+      const updateDoc ={
+        $set:{
+          name: updateClass.name,
+          description: updateClass.description,
+          price : updateClass.price,
+          availableSeats: parseInt(updateClass.availableSeats),
+          image: updateClass.image,
+          videoLink: updateClass.videoLink,
+          status: "pending"
+        }
+      };
+      const result = await classCollection.updateOne(filter,updateDoc,options);
       res.send(result)
     })
 
